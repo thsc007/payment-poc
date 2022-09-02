5f4db924ba0d609a506946d9e75b535c21ecb4a0
thsc <t.schumacher@thsc.ch>
2022-09-02 10:00:15 +0200

    structure

---
+++ src/main/java/ch/thsc/payment/poc/HybridPaymentSolutionPocApplication.java
@@ -0,0 +1,13 @@
+package ch.thsc.payment.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HybridPaymentSolutionPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HybridPaymentSolutionPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/thsc/payment/poc/HybridPaymentSolutionPocApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.thsc.payment.poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HybridPaymentSolutionPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
